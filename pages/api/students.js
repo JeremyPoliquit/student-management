3c12d78360b714c5f4cad05a672683e24deca02f
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2025-01-25 04:14:30 -0800

    get/retrieve data

--- pages/api/students.js
+++ pages/api/students.js
@@ -1,7 +1,15 @@
-import {createStudent} from '../../lib/students';
+import { createStudent, getStudent } from '../../lib/students';
 
 export default async function handler (req, res) {
     switch (req.method) {      
+        case 'GET':
+            try {
+                const students = await getStudent();
+                res.status(200).json(students);
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+            break
         case 'POST':
             const { firstname, lastname, age, gender, contact_number } = req.body;
             try {
